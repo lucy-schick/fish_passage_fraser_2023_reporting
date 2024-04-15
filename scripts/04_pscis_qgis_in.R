82941e9f60e8ed6ed4fdcc3930a56a12fc15fef3
almac2022 <al@newgraphenvironment.com>
2024-04-15 11:49:18 -0700

    link to road_tenure tutorial to help clarify intention of placeholder script. Links to https://github.com/NewGraphEnvironment/fish_passage_template_reporting/issues/3

--- scripts/04_pscis_qgis_in.R
+++ scripts/04_pscis_qgis_in.R
@@ -2,3 +2,7 @@
 # best way to do this is perhaps to do joins and then write back to the csv....
 # just a place holder for now.
 
+# this script gives an example of type of updates we will do https://github.com/NewGraphEnvironment/fish_passage_template_reporting/blob/main/scripts/tutorials/road_tenure.Rmd
+#  this work was previously completed in `scripts/01_prep_inputs` directories of reports.
+
+#
