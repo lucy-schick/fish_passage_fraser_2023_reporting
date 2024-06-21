35fdf6f0210f20a94409048a59cc407fa1caba73
almac2022 <al@newgraphenvironment.com>
2024-06-21 11:29:10 -0700

    update to run for simpcw with results as per https://github.com/NewGraphEnvironment/fish_passage_fraser_2023_reporting/issues/18

--- scripts/01_photos.R
+++ scripts/01_photos.R
@@ -8,8 +8,8 @@ dir_job_id <- '2023-065-sern-capacity'
 dir_photos_mergin_raw <- paste0("~/Projects/gis/", dir_project, "/ignore_mobile/photos/")
 dir_photos_mergin_resized <- paste0("~/Projects/gis/", dir_project, "/ignore_mobile/photos_resized/")
 # dir_photos_processed_final <- paste0("~/Projects/data/", dir_project, "/photos/")
-dir_photos_originals <- paste0("~/Projects/current/", dir_job_id, "/upper_fraser/data/photos/")
-dir_photos_onedrive <- paste0("~/Library/CloudStorage/OneDrive-Personal/Projects/2023_data/upper_fraser/photos/")
+dir_photos_originals <- paste0("~/Projects/current/", dir_job_id, "/fraser/data/photos/")
+dir_photos_onedrive <- paste0("~/Library/CloudStorage/OneDrive-Personal/Projects/2023_data/fraser/photos/")
 
 # we are going to resize the original photos and move them to onedrive just in case we need to do homework
 dir.create(paste0(dir_photos_onedrive, "ai"), recursive = TRUE)
@@ -85,7 +85,7 @@ file.create(
 ########################################################################################################################
 # Rename the photos and sort into directories -----------------------------------------------------------------------------------------------------
 
-# here we assume all sites were comleted digitally. Should confirm...
+
 form_pscis_photos_raw <- sf::st_read(
   dsn = paste0('../../gis/', dir_project, '/data_field/2023/form_pscis_2023.gpkg')) %>%
   arrange(site_id)
@@ -140,33 +140,137 @@ test <- fpr::fpr_photo_qa() %>%
   bind_rows() %>%
   dplyr::filter(if_any(everything(), is.na))
 
+
+
+################################################################################################################
+#--------------------------------------------------Simpc---------------------------------------------------
+################################################################################################################
+
+
+# Build Local Directory  ------------------------------------------------------------
+dir_project <- 'sern_simpcw_2023'
+dir_job_id <- '2023-065-sern-capacity'
+dir_photos_mergin_raw <- paste0("~/Projects/gis/", dir_project, "/ignore_mobile/photos/")
+dir_photos_mergin_resized <- paste0("~/Projects/gis/", dir_project, "/ignore_mobile/photos_resized/")
+# dir_photos_processed_final <- paste0("~/Projects/data/", dir_project, "/photos/")
+dir_photos_originals <- paste0("~/Projects/current/", dir_job_id, "/fraser/data/photos/")
+dir_photos_onedrive <- paste0("~/Library/CloudStorage/OneDrive-Personal/Projects/2023_data/fraser/photos/")
+
+
+# QGIS mergin photos-----------------------------------------------------------------------------------------------------
+## Clean up the mergin file----------------------------------------------------------------------------------------------------
+# remove photos.txt file included in project when created (was to allow mergin git to see the photos dir) but needs
+# to be removed or ignored to not break fpr_photo_resize_batch
+file.remove(
+  paste0(dir_photos_mergin_raw, "/photos.txt")
+)
+
+## Resize----------------------------------------------------------------------------------------------------
+# resize the photos and change the extension to JPG for consistency and to avoid issues with fpr_photo calls in reporting
+# sync to mergin after copying to new dir (resized) and removing originals
+# record version number of mergin project in issue for now to track
+
+# get a list of the photos
+p <- list.files(dir_photos_mergin_raw, full.names = T, recursive = T)
+
+
+# see how large the photos are in MB rounded to 1 decimal using purrr
+s <- p |>
+  purrr::map(file.info) |>
+  purrr::map_dbl("size")/1024/1024
+
+# identify the range of sizes
+range(s)
+# [1] 0.2178898 7.3512239 HUUUUGGGGEEE.  Lets resize
+
+# create the target directory
+dir.create(dir_photos_mergin_resized, recursive = TRUE)
+
+fpr_photo_resize_batch(
+  dir_source = dir_photos_mergin_raw,
+  dir_target = paste0(dir_photos_mergin_resized)
+)
+
+# quick check to see if the photos are all accounted for
+identical(
+  length(
+    list.files(dir_photos_mergin_raw, full.names = T, recursive = T)),
+  length(
+    list.files(dir_photos_mergin_resized, full.names = T, recursive = T))
+)
+
+# erase all the photos in the original directory
+file.remove(
+  list.files(dir_photos_mergin_raw, full.names = T, recursive = T)
+)
+
+# recreate the photos.txt file so the form still works
+file.create(
+  paste0(dir_photos_mergin_raw, "photos.txt")
+)
+
+
+
+
+########################################################################################################################
+# Rename the photos and sort into directories -----------------------------------------------------------------------------------------------------
+path <- "~/Projects/gis/sern_simpcw_2023/data_field/2023/form_pscis_2023.gpkg"
+# here we assume all sites were comleted digitally. Should confirm...
+form_pscis_photos_raw <- fpr::fpr_sp_gpkg_backup(
+  path_gpkg = path,
+  update_utm = TRUE,
+  update_site_id = TRUE,
+  write_back_to_path = FALSE,
+  write_to_csv = FALSE,
+  write_to_rdata = FALSE,
+  return_object = TRUE)
+
+
+# # here we assume all sites were completed digitally. Should confirm...
+# form_pscis_photos_raw <- sf::st_read(
+#   dsn = paste0('../../gis/', dir_project, '/data_field/2023/form_pscis_2023.gpkg')) %>%
+#   arrange(site_id)
+
+# check for duplicate sites
+form_pscis_photos_raw %>%
+  dplyr::filter(!is.na(site_id)) %>%
+  group_by(site_id) %>%
+  dplyr::filter(n()>1) |>
+  nrow()
+
+# check for empty sites
+form_pscis_photos_raw %>%
+  dplyr::filter(is.na(site_id)) %>%
+  nrow()
+
+# create site photo directories right on mergin to make them easy to share...
+form_pscis_photos_raw %>%
+  pull(site_id) %>%
+  as.character() %>%
+  purrr::map(
+    fpr::fpr_photo_folders, path = dir_photos_mergin_raw
+  )
+
+# rename the photos
+fpr::fpr_photo_rename(
+  dat = form_pscis_photos_raw,
+  dir_from_stub = dir_photos_mergin_resized,
+  dir_to_stub = dir_photos_mergin_raw
+)
+
+# QA
+fpr::fpr_photo_qa_df(dat = form_pscis_photos_raw, dir_photos = dir_photos_mergin_raw)
+
+
+fpr_photo_qa_missing_all(
+  dat = form_pscis_photos_raw,
+  dir_photos = dir_photos_mergin_raw
+)
+
+
+
 #############################################################################################
 ####################  NOT RUN YET  ##############################################################
 ##############################################################################################
 # build photo amalgamation for each site ------------------------------------------------
-# can use field form too
-pscis_all <- fpr::fpr_import_pscis_all() %>%
-  bind_rows()
-
-pscis_all %>%
-  distinct(site_id) %>%
-  arrange(site_id) %>%
-  # put this here to work around issue 64
-  # filter(site_id != 198110) %>%
-  # filter(site_id != 123750) %>%
-  # filter(site_id != 198116) %>%
-  # head() %>% #test
-  pull(site_id)  %>%
-  purrr::map(fpr_photo_amalg_cv)
-
-
-# can't build the amalgamated photos
-setdiff(  list.dirs('data/photos', full.names = F, recursive = F),
-
-          pscis_all %>%
-            distinct(site_id) %>%
-            arrange(site_id) %>%
-            # head() %>% #test
-            pull(site_id)
-)
 
