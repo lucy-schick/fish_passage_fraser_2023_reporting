1c79c90a030a33be0ff4f7992c0ac41886e1430c
almac2022 <al@newgraphenvironment.com>
2024-08-25 17:31:49 -0700

    use staticimports

--- scripts/run.R
+++ scripts/run.R
@@ -4,7 +4,9 @@ preview_chapter('0100-intro.Rmd')
 #################################################################################################
 ##go to the index.Rmd and change gitbook_on <- TRUE
 #################################################################################################
-
+staticimports::import()
+source('scripts/staticimports.R')
+my_news_to_appendix()
 rmarkdown::render_site(output_format = 'bookdown::gitbook',
                        encoding = 'UTF-8')
 
