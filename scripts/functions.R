125283aa5857a809531b289725ab8204a09861f1
almac2022 <al@newgraphenvironment.com>
2024-08-25 17:31:39 -0700

    move to staticimports

--- scripts/functions.R
+++ scripts/functions.R
@@ -275,55 +275,5 @@ mygt <- function(x, page_size = 5, font = "10px", ...) {
 }
 
 
-my_dt_table <-   function(dat,
-                          cols_freeze_left = 3,
-                          page_length = 10,
-                          col_align = 'dt-right',
-                          font_size = '10px'){
 
-  dat %>%
-    DT::datatable(
-      style = 'bootstrap',
-      class = 'cell-border stripe', #'dark' '.table-dark',
-      #https://stackoverflow.com/questions/36062493/r-and-dt-show-filter-option-on-specific-columns
-      filter = 'top',
-      extensions = c("Buttons","FixedColumns", "ColReorder"),
-      rownames= FALSE,
-      options=list(
-        scrollX = TRUE,
-        columnDefs = list(list(className = col_align, targets = "_all")), ##just added this
-        pageLength = page_length,
-        dom = 'lrtipB',
-        buttons = c('excel','csv'),
-        fixedColumns = list(leftColumns = cols_freeze_left),
-        lengthMenu = list(c(5,10,25,50,-1),
-                          c(5,10,25,50,"All")),
-        colReorder = TRUE,
-        #https://stackoverflow.com/questions/44101055/changing-font-size-in-r-datatables-dt
-        initComplete = htmlwidgets::JS(glue::glue(
-          "function(settings, json) {{ $(this.api().table().container()).css({{'font-size': '{font_size}'}}); }}"
-        ))
-        #https://github.com/rstudio/DT/issues/1085 - this is not working yet
-        #   initComplete = JS(
-        #     'function() {$("html").attr("data-bs-theme", "dark");}')
-      )
-    )
-}
-
-ltab_caption <- function(caption_text = my_caption) {
-  cat(
-    "<table>",
-    paste0(
-      "<caption>",
-      "(#tab:",
-      # this is the chunk name!!
-      knitr::opts_current$get()$label,
-      ")",
-      caption_text,
-      "</caption>"
-    ),
-    "</table>",
-    sep = "\n"
-  )
-}
 
