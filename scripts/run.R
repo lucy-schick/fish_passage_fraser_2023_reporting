b7130c4d7dc8d29e92c9bf58e334720a88ef27bb
almac2022 <al@newgraphenvironment.com>
2024-11-14 12:38:25 -0800

    switch to sern logo

--- scripts/run.R
+++ scripts/run.R
@@ -4,11 +4,14 @@ preview_chapter('0100-intro.Rmd')
 #################################################################################################
 ##go to the index.Rmd and change gitbook_on <- TRUE
 #################################################################################################
-staticimports::import()
-source('scripts/staticimports.R')
-my_news_to_appendix()
-rmarkdown::render_site(output_format = 'bookdown::gitbook',
-                       encoding = 'UTF-8')
+{
+  staticimports::import()
+  source('scripts/staticimports.R')
+  my_news_to_appendix()
+  rmarkdown::render_site(output_format = 'bookdown::gitbook',
+                         encoding = 'UTF-8')
+
+}
 
 
 {
