e044538bcd0594b6d29b477e72a8de74226666e1
newgraph-lschick <lucy@newgraphenvironment.com>
2025-03-01 13:18:48 -0800

    build photo amalgamation and move to repo for 2023 and 2024 photos - see https://github.com/NewGraphEnvironment/fish_passage_fraser_2023_reporting/issues/118 and https://github.com/NewGraphEnvironment/fish_passage_fraser_2023_reporting/issues/129

--- scripts/02_reporting/0150-build-photo-amalg.R
+++ scripts/02_reporting/0150-build-photo-amalg.R
@@ -1,12 +1,39 @@
 # Build photo amalgamation for each site
 
+###### START OF CODE TO ADDRESS issue https://github.com/NewGraphEnvironment/fish_passage_fraser_2023_reporting/issues/129 ######
+
+# This is a bit funky for fraser 2024 - see issue above
+
+#2024 data
+
 ## Params
-dir_photos_onedrive <- fs::path_expand(fs::path("~/Library/CloudStorage/OneDrive-Personal/Projects", params$job_name, "data/photos/"))
+dir_photos_2024 <- fs::path_expand(fs::path("~/Library/CloudStorage/OneDrive-Personal/Projects", params$job_name, "data/photos/sorted"))
+# form_pscis_2024 is stored in the sqlite
+source("scripts/02_reporting/0120-read-sqlite.R")
 
 
 # get a list of sites to burn
+sites_l <- form_pscis_2024 |>
+  dplyr::distinct(site_id) |>
+  dplyr::arrange(site_id) |>
+  dplyr::pull(site_id)
+
+
+# burn the amalgamated photos to onedrive
+sites_l |>
+  purrr::map(fpr::fpr_photo_amalg_cv, dir_photos = paste0(dir_photos_2024, "/"))
+
+
+#2023 data
+
+## simpcw
+
+# Params
+dir_photos_simpcw_2023 <- fs::path_expand("~/Projects/gis/sern_simpcw_2023/ignore_mobile/photos")
+path_sites_simpcw_2023 <- fs::path_expand("~/Projects/gis/sern_simpcw_2023/data_field/2023/form_pscis_2023.gpkg")
+
 sites_l <- fpr::fpr_sp_gpkg_backup(
-  form_pscis,
+  path_sites_simpcw_2023,
   update_site_id = FALSE,
   write_to_rdata = FALSE,
   write_to_csv = FALSE,
@@ -18,6 +45,40 @@ sites_l <- fpr::fpr_sp_gpkg_backup(
   dplyr::pull(site_id)
 
 
-# burn the amalgamated photos to onedrive
+# burn the amalgamated photos to mergin
 sites_l |>
-  purrr::map(fpr::fpr_photo_amalg_cv, dir_photos = paste0(dir_photos_onedrive, "/"))
+  purrr::map(fpr::fpr_photo_amalg_cv, dir_photos = paste0(dir_photos_simpcw_2023, "/"))
+
+## STILL NEED TO DO sern_lchl_necr_fran_2023 - see issue
+
+###### END OF CODE TO ADDRESS issue https://github.com/NewGraphEnvironment/fish_passage_fraser_2023_reporting/issues/129 ######
+
+
+
+
+
+
+
+## This is the normal code, used when we don't have the issues mentioned above, commenting out for now so we can use it in the next projects
+
+# ## Params
+# dir_photos_onedrive <- fs::path_expand(fs::path("~/Library/CloudStorage/OneDrive-Personal/Projects", params$job_name, "data/photos/"))
+#
+#
+# # get a list of sites to burn
+# sites_l <- fpr::fpr_sp_gpkg_backup(
+#   form_pscis,
+#   update_site_id = FALSE,
+#   write_to_rdata = FALSE,
+#   write_to_csv = FALSE,
+#   write_back_to_path = FALSE,
+#   return_object = TRUE
+# ) |>
+#   dplyr::distinct(site_id) |>
+#   dplyr::arrange(site_id) |>
+#   dplyr::pull(site_id)
+#
+#
+# # burn the amalgamated photos to onedrive
+# sites_l |>
+#   purrr::map(fpr::fpr_photo_amalg_cv, dir_photos = paste0(dir_photos_onedrive, "/"))
