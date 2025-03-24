7fe8f3886537b3b627a0cd2b0172d191ad3d53d5
newgraph-lschick <lucy@newgraphenvironment.com>
2025-03-24 16:58:19 -0700

    Updating with temp function fpr_table_cv_summary_memo_test to overcome issue https://github.com/NewGraphEnvironment/mybookdown-template/issues/65

--- scripts/functions.R
+++ scripts/functions.R
@@ -23,4 +23,39 @@ fif <- function(what, where=".", in_files="\\.[Rr]$", recursive = TRUE,
   if (!found) message("(No results found)")
 }
 
+fpr_table_cv_summary_memo_test <- function(dat = pscis_phase2,
+                                           site = my_site,
+                                           site_photo_id = my_site,
+                                           font = 11,
+                                           col_filter = pscis_crossing_id) {
+
+  dat_site <- dat |> dplyr::filter({{ col_filter }} == site)
+  comments <- dat_site |> dplyr::pull(assessment_comment)
+
+  comment_label <- paste0("Comments: ", comments)
+
+  photo_label <- paste0(
+    "Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet.\n\n",
+    "![](data/photos/", site_photo_id, "/crossing_all.JPG)"
+  )
+
+
+  fpr_kable(
+    fpr_table_cv_detailed(dat = dat_site),
+    caption_text = paste0("Summary of fish passage assessment for PSCIS crossing ", site, "."),
+    booktabs = TRUE,
+    scroll = FALSE,
+    font = font
+  ) |>
+    kableExtra::add_footnote(
+      label = c(comment_label, photo_label),
+      notation = "none",
+      threeparttable = TRUE
+    )
+}
+
+
+
+
+
 
